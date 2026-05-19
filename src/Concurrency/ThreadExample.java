0e27227f88fe22961689ae1ea83414db6fcf3142
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-19 15:53:38 +0200

    using thread class

--- src/Concurrency/ThreadExample.java
+++ src/Concurrency/ThreadExample.java
@@ -1,4 +1,8 @@
 package Concurrency;
 
-public class ThreadExample {
+public class ThreadExample extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Hello from: " + Thread.currentThread().getName());
+    }
 }
--- src/Concurrency/Threads.java
+++ src/Concurrency/Threads.java
@@ -1,4 +1,14 @@
 package Concurrency;
 
 public class Threads {
+    public static void main(String[] args) {
+        ThreadExample first = new ThreadExample();
+        ThreadExample second = new ThreadExample();
+
+        first.setName("First Thread");
+        second.setName("Second Thread");
+
+        first.start();
+        second.start();
+    }
 }
