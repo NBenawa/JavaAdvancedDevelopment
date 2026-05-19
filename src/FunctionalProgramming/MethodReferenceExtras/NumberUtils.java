9e445579e9190ff0347b5d1c4079e928f2627442
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-19 15:52:36 +0200

    practicing generics in java

---
+++ src/FunctionalProgramming/MethodReferenceExtras/NumberUtils.java
@@ -0,0 +1,4 @@
+package FunctionalProgramming.MethodReferenceExtras;
+
+public class NumberUtils {
+}
--- src/Generics/GenericsTwo.java
+++ src/Generics/GenericsTwo.java
@@ -1,4 +1,4 @@
-package generics;
+package Generics;
 
 public class GenericsTwo {
     public static void main(String[] args) {
