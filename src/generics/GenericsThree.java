610402ee95e2a1d1127981bdee85235d1f2c9870
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-19 09:32:30 +0200

    adding generics three class

---
+++ src/generics/GenericsThree.java
@@ -0,0 +1,59 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsThree {
+    public static void main(String[] args) {
+        Shirt shirt = new Shirt();
+        Jacket jacket = new Jacket();
+
+        checkOutItem(shirt);
+        checkOutItem(jacket);
+
+        List<Item> items = new ArrayList<>();
+        items.add(shirt);
+        items.add(jacket);
+
+        checkOutAllItems(items);
+    }
+
+    private static void checkOutAllItems(List<Item> items) {
+        items.forEach(GenericsThree::checkOutItem);
+    }
+
+    public static void checkOutItem (Item item) {
+        System.out.println("Item purchased: " + item.getName() + ", price: " + item.getPrice());
+    }
+}
+
+abstract class Item {
+    abstract Integer getPrice();
+    abstract String getName();
+}
+
+class Shirt extends Item {
+
+    @Override
+    Integer getPrice() {
+        return 10;
+    }
+
+    @Override
+    String getName() {
+        return "Shirt";
+    }
+}
+
+class Jacket extends Item {
+
+    @Override
+    Integer getPrice() {
+        return 30;
+    }
+
+    @Override
+    String getName() {
+        return "Jacket";
+    }
+}
