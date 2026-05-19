84c37f799b1e605b98d13bad4443abeb270a0acc
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-19 09:03:23 +0200

    removing extra Main class and adding genericOne

---
+++ src/generics/GenericsOne.java
@@ -0,0 +1,21 @@
+package generics;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GenericsOne {
+    public static void main(String[] args) {
+        Integer[] numbers = {1, 3, 5, 8};
+        String[] fruits = {"Pear", "Banana", "Apple", "Mango"};
+
+        List<Integer> numbersList = convertToList(numbers);
+        List<String> fruitsList = convertToList(fruits);
+
+        System.out.println(numbersList);
+        System.out.println(fruitsList);
+    }
+
+    private static <T> List<T> convertToList(T[] array) {
+        return Arrays.asList(array);
+    }
+}
